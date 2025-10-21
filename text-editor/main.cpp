32bb1f10808db6bb649d1681983ea4ee5bd69d1a
Daniel Schoonmaker <daniel.schoonmaker@outlook.com>
2025-10-21 09:55:55 -0400

    Link SDL3 and FreeType libraries

--- text-editor/main.cpp
+++ text-editor/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
-int main() {
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <freetype/freetype.h>
+
+int main(int argc, char** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	FT_Library ftLibrary;
+	FT_Error error = FT_Init_FreeType(&ftLibrary);
+	if (error != 0) {
+		exit(1);
+	}
+
+	FT_Done_FreeType(ftLibrary);
+
 	std::cout << "hello world\n";
+
+	SDL_Quit();
+
+	return 0;
 }
